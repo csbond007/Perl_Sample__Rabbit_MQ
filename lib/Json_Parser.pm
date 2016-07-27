87babc5d5df2a684460272e439fc842f64f5e322
Sasikumar Mani <sasikumarmofs1@gmail.com>
2016-07-27 18:04:54 -0400

    JSON  Code Formatting

--- lib/Json_Parser.pm
+++ lib/Json_Parser.pm
@@ -5,39 +5,39 @@ use JSON qw( );
 
 use base 'Exporter';
 
-our@ EXPORT_OK = qw(json_parsing);
+our @EXPORT_OK = qw(json_parsing);
 
 sub json_parsing {
 
-	my ($json_text) = @_;
+    my ($json_text) = @_;
 
-	my $json = JSON->new;
-	my $data = $json->decode($json_text);
+    my $json = JSON->new;
+    my $data = $json->decode($json_text);
 
-	my $From;
-	my $To ;
-	my $Subject ;
-        my $Id ;
-	my $Size ;
+    my $From;
+    my $To;
+    my $Subject;
+    my $Id;
+    my $Size;
 
-        my @email_msg ;
-	
-	for ( @{$data->{alerts}} )
-  		{
-        
-	        $From = $_->{From} ;
-        	$To = $_->{To} ;
-	        $Subject = $_->{Subject} ;
-		$Id = $_->{Id} ;
-	        $Size = $_->{Size} ;
-	
-		my @part_msg;
+    my @email_msg;
 
-	        push @part_msg , $From , $To , $Subject , $Id , $Size ;
+    for ( @{ $data->{alerts} } ) {
 
-        	push(@email_msg, @part_msg);
-		
-		}
+        $From    = $_->{From};
+        $To      = $_->{To};
+        $Subject = $_->{Subject};
+        $Id      = $_->{Id};
+        $Size    = $_->{Size};
 
-	return @email_msg ;
-} # end json_parsing
+        my @part_msg;
+
+        push @part_msg, $From, $To, $Subject, $Id, $Size;
+
+        push( @email_msg, @part_msg );
+
+    }
+
+    return @email_msg;
+}    # end json_parsing
+
