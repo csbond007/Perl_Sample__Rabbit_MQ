d008ea6049d785d32e01cb3e6e73f877f0679670
Kaustav Saha <kaustav.edu@gmail.com>
2016-07-28 10:57:26 -0400

    Commit_28_1

---
+++ lib/RabbitMQ_new_task.pm
@@ -0,0 +1,56 @@
+
+package RabbitMQ_Receive;
+use strict;
+use warnings;
+use Net::RabbitFoot;
+use AnyEvent;
+$|++;
+
+use InfluxDB_Operations qw(write);
+use AWS qw(put_S3);
+
+use base 'Exporter';
+
+our @EXPORT_OK = qw(receive);
+
+sub receive {
+
+    my $conn = Net::RabbitFoot->new()->load_xml_spec()->connect(
+        host  => 'localhost',
+        port  => 5672,
+        user  => 'guest',
+        pass  => 'guest',
+        vhost => '/',
+    );
+
+    my $ch = $conn->open_channel();
+
+    $ch->declare_queue(
+        queue   => 'hello1',
+        durable => 1,
+    );
+
+    print " [*] Waiting for messages. To exit press CTRL-C\n";
+
+    sub callback {
+        my $var  = shift;
+        my $body = $var->{body}->{payload};
+        print " [x] Received \n";    # $body \n";
+        
+        #Write the records into InfluxDB
+        write($body);
+ 	
+	#Write the files into AWS-S3 buckets
+        put_S3($body);
+    }
+
+    $ch->consume(
+        on_consume => \&callback,
+        no_ack     => 1,
+    );
+
+    AnyEvent->condvar->recv;
+
+}  # end receive
+
+
