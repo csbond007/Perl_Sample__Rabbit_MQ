8c6b6357586f0d624c057892a5bdbfdcd8b4f3fc
Sasikumar Mani <sasikumarmofs1@gmail.com>
2016-07-28 15:10:12 -0400

    Adding Configurations

--- lib/RabbitMQ_Receive.pm
+++ lib/RabbitMQ_Receive.pm
@@ -7,8 +7,9 @@ use AnyEvent;
 $|++;
 
 use InfluxDB_Operations qw(write);
-use AWS qw(put_S3);
 use Config_Reader qw(getConfigValueByKey);
+use AWS qw(put_S3);
+
 
 use base 'Exporter';
 
@@ -17,17 +18,17 @@ our @EXPORT_OK = qw(receive);
 sub receive {
 
     my $conn = Net::RabbitFoot->new()->load_xml_spec()->connect(
-        host  => getConfigValueByKey("rabbitMQHost");,
-        port  => getConfigValueByKey("rabbitMQport");,
-        user  => getConfigValueByKey("rabbitMQuser");,
-        pass  => getConfigValueByKey("rabbitMQpass");,
+        host  => getConfigValueByKey("rabbitMQHost"),
+        port  => getConfigValueByKey("rabbitMQport"),
+        user  => getConfigValueByKey("rabbitMQuser"),
+        pass  => getConfigValueByKey("rabbitMQpass"),
         vhost => '/',
     );
 
     my $ch = $conn->open_channel();
 
     $ch->declare_queue(
-        queue   => 'hello1',
+        queue   => getConfigValueByKey("rabbitMQName"),
         durable => 1,
     );
 
@@ -36,7 +37,7 @@ sub receive {
     sub callback {
         my $var  = shift;
         my $body = $var->{body}->{payload};
-        print " [x] Received \n";    # $body \n";
+        print " [x] Received \n";
         
         #Write the records into InfluxDB
         write($body);
