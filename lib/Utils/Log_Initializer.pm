78e968279ee93e1f674ddc21ff224dd9980c8378
Sasikumar Mani <sasikumarmofs1@gmail.com>
2016-08-02 11:58:26 -0400

    Logging configurations

--- lib/Utils/Log_Initializer.pm
+++ lib/Utils/Log_Initializer.pm
@@ -4,16 +4,16 @@ use warnings;
 use Config::Any;
 
 use Log::Log4perl;
+
 # Initialize Logger
-my $log_conf = "./Logs/log4perl.conf";
+my $log_conf = "./config/log4perl.conf";
 Log::Log4perl::init($log_conf);
 my $logger = Log::Log4perl->get_logger();
 
 use base 'Exporter';
 our @EXPORT_OK = qw(get_Logger);
 
-sub get_Logger 
-  {
-    return $logger ;
-  }
+sub get_Logger {
+    return $logger;
+}
 
