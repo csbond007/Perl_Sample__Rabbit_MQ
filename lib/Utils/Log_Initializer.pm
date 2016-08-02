d6449df9e7df123a067a8c651e0d9f2bd1cb1910
Kaustav Saha <kaustav.edu@gmail.com>
2016-08-02 11:31:43 -0400

    commit_08_02_2

---
+++ lib/Utils/Log_Initializer.pm
@@ -0,0 +1,19 @@
+package Log_Initializer;
+use strict;
+use warnings;
+use Config::Any;
+
+use Log::Log4perl;
+# Initialize Logger
+my $log_conf = "./Logs/log4perl.conf";
+Log::Log4perl::init($log_conf);
+my $logger = Log::Log4perl->get_logger();
+
+use base 'Exporter';
+our @EXPORT_OK = qw(get_Logger);
+
+sub get_Logger 
+  {
+    return $logger ;
+  }
+
